781f0892866af0b26de51c721c93a2eae4373974
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-09-23 14:38:50 +0400

    finished form page and messige is sand and diplayed properly

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-const homeRouter = require("./routes/homeRoute");
-const messageRouter = require("./routes/messageRoute");
+// const homeRouter = require("./routes/homeRoute");
+// const messageRouter = require("./routes/messageRoute");
 
 const path = require("node:path");
 // serving static assets
@@ -29,8 +29,19 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
-app.get("/message", (req, res) => {
-  res.render("message", { messages });
+app.get("/new", (req, res) => {
+  res.render("form", { title: "Mini Messageboard" });
+});
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/new", (req, res) => {
+  const message = {
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  };
+  messages.push(message);
+  res.redirect("/");
 });
 
 // app.use("/", homeRouter);
